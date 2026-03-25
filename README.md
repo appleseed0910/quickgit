39146c753045398268fdf1cd5d1f1522955da5c4
appleseed0910 <xinyi.hu01@sap.com>
2026-03-25 12:28:42 +0800

    add empty commit msg fallback and error handling

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # quickgit
 
-TODO:
+Done: 
 
-1. 防命令注入fix
+1. 防命令注入fix, command injection. [使用 execFile代替exec]
+2. 空参数的process.exit 和 归并退出至 catch block 中
 
 ---
 
-2. 多参数enhance 比如说 prefix
-3. 实时print out cmd输出
-4. 空参数的return fallback
+TODO:
+
+1. 多参数enhance 比如说 prefix
+2. 实时print out cmd输出
--- bin/index.js
+++ bin/index.js
@@ -4,9 +4,13 @@ import { exec, execFile } from 'child_process'
 import { promisify } from 'util'
 
 const execFilePromise = promisify(execFile)
-const msg = process.argv[2]
+const msg = process.argv[2] ?? ''
 
 try {
+    if (!msg) {
+        throw new Error('Empty commit message!') 
+    }
+
     await execFilePromise('git', ['add', ':/'])
     const { stdout: commitout, stderr: commiterr } = await execFilePromise('git', ['commit', '-m', msg])
     const { stdout, stderr } = await execFilePromise('git', ['push'])
@@ -16,4 +20,5 @@ try {
     if (stderr) console.error(stderr);
 } catch (e) {
     console.error(e?.message);
+    process.exit(1)
 }
